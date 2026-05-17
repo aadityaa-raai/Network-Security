e8544419faba8ce3c449d08c8418b3b439d9a12e
aadityaa-raai <aadityaa.raai@gmail.com>
2026-05-17 23:23:45 +0530

    Model Trainer Debugging

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
@@ -38,6 +39,13 @@ if __name__=="__main__":
         logging.info("Data Transformation Completed")
 
         print(data_transformation_artifact)
+
+        model_trainer_config=ModelTrainerConfig(training_pipeline_config)
+        model_trainer=ModelTrainer(model_trainer_config,data_transformation_artifact)
+        
+        logging.info("Initiate Model Training")
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Model Training Completed")
 
 
 
